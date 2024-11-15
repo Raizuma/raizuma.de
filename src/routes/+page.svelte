1f460a7f34d12963d8a6a216cb71a281f8a97d32
Nagisa7Shiota <121880460+Nagisa7Shiota@users.noreply.github.com>
2024-11-15 17:44:07 +0100

    Startseite
    - Neue Kategorien hinzugefügt.
    -  Basis der Preview ist da.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<div class="text-4xl text-center">Raizuma - So schnell wie der Blitz</div>
-<div class="text-xl text-center">Das Team der Ewigkeit und deutschen Übersetzungen</div>
+<div>
+    <div class="text-4xl text-center">Raizuma - So schnell wie der Blitz</div>
+    <div class="text-xl text-center">Das Team der Ewigkeit und deutschen Übersetzungen</div>
+</div>
+<div>
+    <div class="text-4xl text-center">Aktuelle Projekte</div>
+    <!-- Letzte Projekte -->
+    <div class="flex flex-col md:flex-row text-center content-center items-center justify-around p-4">
+        <a href="/contact" class="card card-hover block variant-ghost-surface">
+            <header class="card-header">
+                <enhanced:img src="$images/deltarune.png" alt="Deltarune" style="width: 210px; border-radius: 10%;" />
+            </header>
+            <section class="text-5xl p-4 font-bold">Deltarune</section>
+            <footer class="card-footer text-xs">Zuletzt aktualisiert: 15.11.2024</footer>
+        </a>
+        <a href="/contact" class="card card-hover block variant-ghost-surface">
+            <header class="card-header">
+                <enhanced:img src="$images/deltarune.png" alt="Deltarune" style="width: 210px; border-radius: 10%;" />
+            </header>
+            <section class="text-5xl p-4 font-bold">Deltarune</section>
+            <footer class="card-footer text-xs">Zuletzt aktualisiert: 15.11.2024</footer>
+        </a>
+        <a href="/contact" class="card card-hover block variant-ghost-surface">
+            <header class="card-header">
+                <enhanced:img src="$images/deltarune.png" alt="Deltarune" style="width: 210px; border-radius: 10%;" />
+            </header>
+            <section class="text-5xl p-4 font-bold">Deltarune</section>
+            <footer class="card-footer text-xs">Zuletzt aktualisiert: 15.11.2024</footer>
+        </a>
+    </div>	
+</div>
+<div>
+    <div class="text-4xl text-center">News Feed</div>
+    <!-- Social Media Feed -->
+</div>
