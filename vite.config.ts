aa9526c4b9f65164dd280c11756a503538d90bc9
Nagisa7Shiota <121880460+Nagisa7Shiota@users.noreply.github.com>
2025-02-20 17:06:03 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
     const serverConfig: CommonServerOptions = {
         // @ts-ignore
         https: true,
-        host: currentHost,
+        host: "0.0.0.0",
         port: 3000,
         strictPort: true,
         proxy: {}
